b983d5cfdc92327e481a5a4a36e6dfa179c9eb5f
Davyd Maker <davydnascimento.englobar@gmail.com>
2020-03-17 15:33:11 -0300

    [feature/16] Emissões | Listagem de Emissões do Fornecedor

---
+++ src/app/components/pages/emissions/interfaces.ts
@@ -0,0 +1,8 @@
+export interface IEmission {
+    id: number;
+    name_passenger: string,
+    email_passenger: string,
+    phone_passenger: string,
+    sale_date: string,
+    date_boarding: string,
+}
