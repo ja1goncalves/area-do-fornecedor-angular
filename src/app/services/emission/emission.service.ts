c888b0002e828b5dfc5306ef9ec1e83efb1a4c9d
Davyd Maker <davydnascimento.englobar@gmail.com>
2020-03-20 17:18:42 +0000

    [78] Listagem de Emissões | Enviar CPF do fornecedor como parâmetro GET

--- src/app/services/emission/emission.service.ts
+++ src/app/services/emission/emission.service.ts
@@ -11,6 +11,6 @@ export class EmissionService {
   constructor(private http: HttpClient) { }
 
   public getEmissions(cpf:number): Observable<any> {
-    return this.http.get(`${environment.API_URL}/api/provider/emissions/${cpf}?limit=100&page=1`);
+    return this.http.get(`${environment.API_URL}/api/provider/emissions?cpf=${cpf}&limit=100&page=1`);
   }
 }
